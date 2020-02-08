5152d3010d56a4541d7ed312df1097ba818980d3
Aquadrone Jetson On-Board <aquadrone@jetson.com>
2020-02-08 18:31:46 -0500

    Clean build and dev files

--- catkin_ws/build/imu_bno055/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/aquadrone/aquadrone2020jetson/catkin_ws/src/ros-imu-bno055-master/imu_bno055/include".split(';') if "/home/aquadrone/aquadrone2020jetson/catkin_ws/src/ros-imu-bno055-master/imu_bno055/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "imu_bno055"
-PROJECT_SPACE_DIR = "/home/aquadrone/aquadrone2020jetson/catkin_ws/devel/.private/imu_bno055"
-PROJECT_VERSION = "1.0.0"
--- catkin_ws/build/imu_bno055/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/aquadrone/aquadrone2020jetson/catkin_ws/install/include".split(';') if "/home/aquadrone/aquadrone2020jetson/catkin_ws/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "imu_bno055"
-PROJECT_SPACE_DIR = "/home/aquadrone/aquadrone2020jetson/catkin_ws/install"
-PROJECT_VERSION = "1.0.0"
